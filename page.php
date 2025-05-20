ce42f68dfe8a661434c3710a40119813a04678cb
Nathgouvea <132577952+Nathgouvea@users.noreply.github.com>
2025-05-20 12:33:06 +0100

    removed the html files

--- page.php
+++ page.php
@@ -5,28 +5,27 @@
     // Get the current page slug
     $page_slug = get_post_field('post_name', get_post());
     
-    // Map WordPress pages to original HTML files
+    // Map WordPress pages to their specific templates
     $page_mapping = array(
-        'aromas' => 'aromas.html',
-        'acessorios' => 'acessorios.html',
-        'aromatizadores' => 'aromatizadores.html',
-        'comprar' => 'comprar.html',
-        'contato' => 'contato.html',
-        'velas-aromaticas' => 'velas-aromaticas.html',
-        'lembrancinhas' => 'lembrancinhas.html',
-        'kits-especiais' => 'kits-especiais.html',
-        'home-spray' => 'home-spray.html',
-        'inicio' => 'index.html'
+        'aromas' => 'page-aromas.php',
+        'acessorios' => 'page-acessorios.php',
+        'aromatizadores' => 'page-aromatizadores.php',
+        'comprar' => 'page-comprar.php',
+        'contato' => 'page-contato.php',
+        'velas-aromaticas' => 'page-velas-aromaticas.php',
+        'lembrancinhas' => 'page-lembrancinhas.php',
+        'kits-especiais' => 'page-kits-especiais.php',
+        'home-spray' => 'page-home-spray.php'
     );
 
-    // If we have a mapping for this page, include the original HTML file
+    // If we have a specific template for this page, use it
     if (isset($page_mapping[$page_slug])) {
-        include get_template_directory() . '/' . $page_mapping[$page_slug];
+        get_template_part('page', $page_slug);
     } else {
         // Fallback to default WordPress content
         while (have_posts()) :
             the_post();
-            the_content();
+            get_template_part('template-parts/content', 'page');
         endwhile;
     }
     ?>
